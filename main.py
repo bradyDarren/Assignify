82de9b2434cad788d9e01f4ba67074e799531f40
bradyDarren <dcgbfinancial@gmail.com>
2025-04-08 22:24:08 -0500

    installation of the pandas module

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import random
 
 # reading of our CSV file containing partner information
 partner_data = pd.read_csv(filepath_or_buffer = "Partners.csv")
+print(partner_data)
 
 # Create a dictionary from the Partner.csv file.
 partner_dict = {row.Name:{'Shift':row.Team, "Lift": row.lift_cert} for (_, row) in partner_data.iterrows()}
