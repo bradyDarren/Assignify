bb0497bf5755fefe285cb2e2c179365914eb1001
Darren Brady <dcgbfinancial@gmail.com>
2025-04-13 05:56:04 +0000

    addition of the employee_list function

--- partners.py
+++ partners.py
@@ -3,8 +3,18 @@ import random
 
 class Partners: 
 
-    def employee_list(self):
-        pass
+    def __init__(self, employee_data):
+        self.employee_data = employee_data
+
+# returns a list of partners for the specified shift
+    def employee_list(self, shift = None):
+        e_list = []
+        for id in self.employee_data:
+            if self.employee_data[id]['Shift'] == shift:
+                e_list.append(self.employee_data[id]["Name"])
+        return e_list
+                
+
 
     def lift_certified(Self):
         pass
