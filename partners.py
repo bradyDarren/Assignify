45b968500627f958a456894c2fdf606deedd0511
Darren Brady <dcgbfinancial@gmail.com>
2025-05-02 05:27:33 +0000

    implementation of the ValueError for the employee_list method and the lift_certified method

--- partners.py
+++ partners.py
@@ -7,15 +7,27 @@ class Partners:
 # returns a list of partners for the specified shift
     def employee_list(self, shift = None):
         e_list = []
+        shift_found = False
         for id in self.employee_data:
             if self.employee_data[id]['Shift'] == shift:
                 e_list.append(self.employee_data[id]["Name"])
+                shift_found = True
+
+        if not shift_found: 
+            raise ValueError("Selected shift does not exist within the database")
+        
         return e_list
 # returns a list of certifed or uncertified forklift employees           
     def lift_certified(self, status):
         cert_list = []
+        status_found = False
         for id in self.employee_data:
             if self.employee_data[id]['Lift'] == status:
                 cert_list.append(self.employee_data[id]['Name'])
-        return cert_list
+                status_found = True
         
+        if not status_found: 
+            raise ValueError("Selected status does not exist within the database")
+        
+        return cert_list
+
