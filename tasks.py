7dc6cc16db72943ee45062dd2ff0e1061864bd76
Darren Brady <dcgbfinancial@gmail.com>
2025-05-02 05:37:05 +0000

    addition of the OOP to read in the Assignments.csv file and create a dictionary from the assignment file

--- tasks.py
+++ tasks.py
@@ -1,5 +1,26 @@
+import pandas as pd
+
 class Tasks:  
-       
-    def task_list(self):
-        pass
+
+    def __init__(self, assignment_data):
+        self.assignment_data = assignment_data
+        self.df = None
+
+    def read_assignment(self):
+        self.df = pd.read_csv(filepath_or_buffer = self.assignment_data)
+        return self.df
     
+    def task_list(self):
+        
+        if self.df is None: 
+            raise ValueError("Assignment Data has note been read yet. Please call the read_assignment() first.")
+        
+        required_columns = ["Quantity", "Assignment"]
+        for col in required_columns: 
+            if col not in self.df.columns: 
+                raise ValueError(f"Column {col} is missing from the dataset, Please ensure it is included.")
+        
+        assignment_dict = {row.Assignment:row.Quantity 
+                           for (_, row) in self.df.iterrows()}
+        
+        return assignment_dict
